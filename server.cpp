bfbd1a1575bb368d572b902fc31ffa0d05958325
Viktor Kopp <vifactor@gmail.com>
2024-11-04 22:28:13 +0100

    A bit of socket programming to establish simple connection

--- server.cpp
+++ server.cpp
@@ -5,6 +5,11 @@
 #include <csignal>
 #include <iostream>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+
 using namespace std::chrono_literals;
 
 namespace {
@@ -21,6 +26,51 @@ void Server::serve()
             std::cout << "Interrupt signal (" << signum << ") received.\n";
             done = true;
         });
-        std::this_thread::sleep_for(100ms);
+
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            std::cerr << "ERROR opening socket";
+            break;
+        }
+
+        struct sockaddr_in serv_addr;
+        memset((void *) &serv_addr, 0, sizeof(serv_addr));
+        const int portno = 1025;
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.sin_port = htons(portno);
+
+        if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            std::cerr << "ERROR on binding" << std::endl;
+            break;
+        }
+        listen(sockfd, 5);
+
+        struct sockaddr_in cli_addr;
+        socklen_t clilen = sizeof(cli_addr);
+        int newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0) {
+            std::cerr << "ERROR on accept" << std::endl;
+            break;
+        }
+
+        char buffer[256];
+        memset(buffer, 0, sizeof(buffer));
+        int n = read(newsockfd,buffer,255);
+        if (n < 0) {
+            std::cerr << "ERROR reading from socket" << std::endl;
+            break;
+        }
+
+        std::cout << "Here is the message: \n" << std::string_view(buffer, n);
+
+        const std::string replyMsg{"I got your message\n"};
+        n = write(newsockfd, replyMsg.data(), replyMsg.size());
+        if (n < 0) {
+            std::cout << "ERROR writing to socket" << std::endl;
+            break;
+        }
+        close(newsockfd);
+        close(sockfd);
     }
 }
